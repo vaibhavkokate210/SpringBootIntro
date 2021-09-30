dfc7805c8ba3dfd7239ad046eaece1bde7998c06
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 04:11:42 +0530

    [Vaibhav] added new spring project is created

--- main/java/com/bridgelabz/springconcept/SpringCoreConceptsApplication.java
+++ main/java/com/bridgelabz/springconcept/SpringCoreConceptsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoreConceptsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from bridgelabz");
 		SpringApplication.run(SpringCoreConceptsApplication.class, args);
 	}
 
